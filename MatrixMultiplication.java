dc1ca18606fa08bbd22ca877210f6a7423ddc317
n1ved <nivedrsalini@gmail.com>
2023-11-18 15:53:02 +0530

    Add Matrix Multiplication

---
+++ MatrixMultiplication.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter row and column of Matrix A : ");
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+        System.out.print("Enter row and column of Matrix B : ");
+        int n1 = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        if(n1 != n){
+            System.out.println("Err : Cannot prerform operation");
+        }
+        else{
+            System.out.println("Enter Matrix A["+m+"]["+n+"]");
+            int[][] A = new int[m][n];
+            for(int  i=0; i<m;i++){
+                for(int j=0; j<n;j++){
+                    A[i][j] = scanner.nextInt();
+                }
+            }
+
+            System.out.println("Enter Matrix A["+n+"]["+k+"]");
+            int[][] B = new int[n][k];
+            for(int  i=0; i<n;i++){
+                for(int j=0; j<k;j++){
+                    B[i][j] = scanner.nextInt();
+                }
+            }
+
+            int[][] R = new int[m][k];
+
+            for(int i=0 ; i<m ;i++){
+                for(int j=0 ; j<k ; j++){
+                    R[i][j] = 0;
+                    for(int l=0 ; l<n ; l++){
+                       R[i][j] = R[i][j] + (A[i][l] * B[l][j]); 
+                    }
+                }
+            }
+            System.out.println("Resultant Matrix");
+            for(int i=0 ; i<m ;i++){
+                for(int j=0 ; j<k ; j++){
+                    System.out.print(R[i][j]+" ");
+                }
+                System.out.println();
+            }
+
+        }
+        scanner.close();
+    }
+}
